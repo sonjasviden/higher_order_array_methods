a22384ab35ad203bbef2e559cd797cbe56e2df18
sonjasviden <sonja.sviden@live.com>
2022-11-15 10:30:34 +0100

    methods

---
+++ sort.js
@@ -0,0 +1,20 @@
+/**
+ * Higher Order Array Methods
+ *
+ * .sort()
+ * .filter()
+ * .find()
+ * .map()
+ * .reduce()
+ *
+ * - Clone an array
+ * - Shuffle an array
+ */
+
+// names
+const names = ["Johan", "Fredrik", "Saman", "Alicia", "Maja", "Elliot"];
+console.log("Names in chaos:", names);
+
+names.sort();
+
+console.log(names);
